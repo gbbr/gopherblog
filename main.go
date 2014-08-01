1d0f7c3aed6b9e28ffd6a5902b2cad96f8c6d995
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-08-01 21:02:45 +0100

    Basic setup

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"github.com/backslashed/gopherblog/helpers"
+	"github.com/backslashed/gopherblog/blog"
 	"net/http"
 )
 
-const (
-	HOSTNAME = "localhost"
-	PORT     = "8080"
-)
+var config = blog.Config{
+	Host:     "localhost",
+	Port:     "8080",
+	DbString: "root:root@tcp(localhost:3306)/blog",
+}
 
 func main() {
-	err := http.ListenAndServe(HOSTNAME+":"+PORT, nil)
-	helpers.HandleError(err, "Error starting server")
+	err := blog.Start(config)
+	blog.HandleError(err, "Error starting blog instance")
+
+	http.Handle("/post/", new(blog.Post))
+
+	err = http.ListenAndServe(config.Host+":"+config.Port, nil)
+	blog.HandleError(err, "Error starting HTTP server")
 }
