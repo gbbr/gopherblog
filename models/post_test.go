810d9eb31a326d1d7bd71e7376f7dbe5d9bf653f
gbbr <gabriel@stripetree.co.uk>
2014-08-14 22:45:01 +0100

    Added tests for models - Post

---
+++ models/post_test.go
@@ -0,0 +1,93 @@
+package models
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"testing"
+)
+
+var (
+	// Test configuration, db connections, schema files
+	testConfig = struct {
+		schemaFile string
+		user, pass string
+		dbString   string
+	}{
+		schemaFile: "../sql/schema_test.sql",
+		user:       "root",
+		pass:       "root",
+		dbString:   "root:root@tcp(localhost:3306)/blog_test",
+	}
+
+	// Sync helper to run function only once
+	once sync.Once
+)
+
+// Sets up test DB from schema file
+func setUp() {
+	file, err := os.Open(testConfig.schemaFile)
+	if err != nil {
+		log.Fatal("Error opening file")
+	}
+	defer file.Close()
+
+	cmd := exec.Command("mysql", "--user="+testConfig.user, "--password="+testConfig.pass)
+	cmd.Stdin = file
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestPostsWithLimit(t *testing.T) {
+	once.Do(setUp)
+
+	ConnectDb(testConfig.dbString)
+	defer CloseDb()
+
+	// Test retrieval of all (considering there's under 100)
+	posts, err := Posts(100)
+	if len(posts) != 7 || err != nil {
+		if err != nil {
+			t.Log(err)
+		} else {
+			t.Logf("Expected %d posts, but got %d", 7, len(posts))
+		}
+
+		t.Fail()
+	}
+
+	// Test limited retrieval
+	posts, err = Posts(3)
+	if len(posts) != 3 || err != nil {
+		if err != nil {
+			t.Log(err)
+		} else {
+			t.Logf("Expected %d posts, but got %d", 3, len(posts))
+		}
+
+		t.Fail()
+	}
+}
+
+func TestPostsByUser(t *testing.T) {
+	once.Do(setUp)
+
+	ConnectDb(testConfig.dbString)
+	defer CloseDb()
+
+	posts, err := PostsByUser(&User{Id: 1})
+	if posts[0].Draft != true || posts[4].Slug != "slug-one" ||
+		len(posts) != 5 || err != nil {
+
+		if err != nil {
+			t.Log(err)
+		} else {
+			t.Log("Returned incorrect posts")
+		}
+
+		t.Fail()
+	}
+}
