283dfe647938b4418b86c9523c6e208a23791433
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-08-19 21:54:07 +0100

    Added a test for users

--- models/post_test.go
+++ models/post_test.go
@@ -1,45 +1,6 @@
 package models
 
-import (
-	"log"
-	"os"
-	"os/exec"
-	"sync"
-	"testing"
-)
-
-var (
-	// Test configuration, db connections, schema files
-	testConfig = struct {
-		schemaFile string
-		user, pass string
-		dbString   string
-	}{
-		schemaFile: "../sql/schema_test.sql",
-		user:       "root",
-		pass:       "root",
-		dbString:   "root:root@tcp(localhost:3306)/blog_test",
-	}
-
-	// Sync helper to run function only once
-	once sync.Once
-)
-
-// Sets up test DB from schema file
-func setUp() {
-	file, err := os.Open(testConfig.schemaFile)
-	if err != nil {
-		log.Fatal("Error opening file")
-	}
-	defer file.Close()
-
-	cmd := exec.Command("mysql", "--user="+testConfig.user, "--password="+testConfig.pass)
-	cmd.Stdin = file
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+import "testing"
 
 func TestPostsWithLimit(t *testing.T) {
 	once.Do(setUp)
@@ -77,7 +38,7 @@ func TestPostsByUser(t *testing.T) {
 	}
 }
 
-func TestFetch(t *testing.T) {
+func TestPostFetch(t *testing.T) {
 	once.Do(setUp)
 
 	ConnectDb(testConfig.dbString)
